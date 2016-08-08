0a91fb5c8ae3dc350c3e2aee98843e27bf62be83
Vigneshwaran007 <vigneshwaranown@gmail.com>
2016-08-08 14:23:01 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;	
+	public class Prime{
+		public static void main(String args[]){
+			Scanner s=new Scanner(System.in);
+			int a,b[];
+			
+			for(int i=0;i<5;i++){
+				a=s.nextInt();
+			}
+			for(int i=0;i<5;i++){
+				if(b[i]<100){
+					char []ch=String.valueOf(b[i]).toCharArray();
+					System.out.println(ch[i]+""+ch[i+1]);
+				}
+			}
+		}
+	}
