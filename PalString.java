8c948d1cbcfcc113afd422a7f1c14200f675b086
Vigneshwaran007 <vigneshwaranown@gmail.com>
2016-08-18 17:36:49 +0530

    Update PalString.java

--- PalString.java
+++ PalString.java
@@ -7,20 +7,20 @@ public class PalString {
 		String s;
 		System.out.println("enter string");
 		
-		s=s1.nextLine();
+		s=s1.next();
 		StringBuffer bf=new StringBuffer(s);
 		StringBuffer bf1=new StringBuffer();
 		String s2=bf.reverse()+"";
 		for(int i=0;i<s.length()-1;i++){
 			if(!s.equals(s2)){
-				System.out.println(s);
-				break;
+				System.out.println(s.length());
+				System.exit(0);
 			}
 			else{
 				bf1.append(s.charAt(i));
 			}
 		}
-		System.out.println(bf1);
+		System.out.println(bf1.length());
 	}
 
 }
