a400bc70e634da97359261bf652f1b9bc561e5ad
Vigneshwaran007 <vigneshwaranown@gmail.com>
2016-08-22 15:41:57 +0530

    Create NumPalindrom.java

---
+++ NumPalindrom.java
@@ -0,0 +1,29 @@
+public class NumPalindrom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="565";
+		int b=Integer.parseInt(a);
+		int sum=0;
+		while(b>0){
+			int temp=b%10;
+			sum+=temp;
+			b=b/10;
+		}
+		System.out.println(sum);
+		String s=""+sum;
+		System.out.println(s);
+		StringBuffer s1=new StringBuffer(s);
+		s1.reverse();
+		System.out.println(s1);
+		String s2=s1+"";
+		System.out.println(s2);
+		if(s.equals(s2)){
+			System.out.println("palindrom");
+		}
+		else{
+			System.out.println("not palindrom");
+		}
+	}
+
+}
